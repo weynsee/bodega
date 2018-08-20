3e96777343c2941bfc9f2e2a1b8c2b5725d02229
Wayne See <wsee@zendesk.com>
2018-08-20 22:25:29 +1000

    Group advances together in receipt

--- app/helpers/payslips_helper.rb
+++ app/helpers/payslips_helper.rb
@@ -1,24 +1,10 @@
 module PayslipsHelper
   def advance_billing_item_label(advance)
-    description = "(#{advance.description})" if advance.description.present?
+    description = "- #{advance.description}" if advance.description.present?
     <<-ITEM
-    #{advance.advance_type.humanize} salary advance
-    on
     #{advance.issue_date.strftime('%m/%d')}
     #{description}
     ITEM
-  end
-
-  def each_advance_billing_item(advances)
-    rest = []
-    advances.each_with_index do |advance, index|
-      if index < 5
-        yield advance
-      else
-        rest << advance
-      end
-    end
-    yield rest if rest.present?
   end
 
   def format_attendance(payslip)
